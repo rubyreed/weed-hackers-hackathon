770f55106c790dc89261996d95aa5bf2d27f246e
Ruby Reed <rubysreed@gmail.com>
2021-12-03 15:03:35 -0700

    controllers and models done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'database_cleaner'
+DatabaseCleaner.clean_with(:truncation)
+
+
+category1 = Category.create(name: "Granny Smith", description: "green, sour, and crisp")
+category2 = Category.create(variety: "Honeycrisp", description: "golden, sweet and crisp")
+
+category1.items.create(species: "category Maggot", description: "likes fallen categorys, does not have legs")
+category1.items.create(species: "Codling Moth", description: "very destructive, native of Eurasia")
+category2.items.create(species: "Plum Curculio", description: "looks like a beetle, consumes seeds")
+category2.items.create(species: "Fruit Fly", description: "small, black")
+
+Job.create(variety: "Asian", description: "large, juicy, best eaten raw")
+Job.create(variety: "Bartlett", description: "yellow when ripe, buttery texture")
