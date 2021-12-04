a7c4faf450d6c2cc8815a3e7eb1e57ccdd406a4d
Michael Knutson <mrknutson44@gmail.com>
2021-12-04 08:54:54 -0900

    basic items index page started

--- client/src/components/Items.js
+++ client/src/components/Items.js
@@ -1,6 +1,19 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const Items = () => {
+  const [items, setItems] = useState([]);
+
+  useEffect(()=>{
+    getData();
+  },[])
+
+  const getData = async ()=> {
+    let res = await axios.get(`/api/categories/1/items`)
+    setItems(res.data)
+    console.log(items)
+  };
+
   return (
     <div>
       <h1>List of Items</h1>
