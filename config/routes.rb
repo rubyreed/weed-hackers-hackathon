017b3438253fe73d68956a9f08f17884558fb5e3
Michael Knutson <mrknutson44@gmail.com>
2021-12-04 08:05:46 -0900

    fixed routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 namespace :api do 
-  resource :categories do
-    resource :items
+  resources :categories do
+    resources :items
   end
-  resource :jobs
+  resources :jobs
 end
 end
