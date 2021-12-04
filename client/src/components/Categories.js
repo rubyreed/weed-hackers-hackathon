b6fad7f7e05bb299e3177f8ea98444d4e73513f5
Ruby Reed <rubysreed@gmail.com>
2021-12-04 10:00:51 -0700

    filled out category add

--- client/src/components/Categories.js
+++ client/src/components/Categories.js
@@ -1,9 +1,34 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import Category from "./Category";
+import axios from "axios";
 
 const Categories = () => {
+
+const [categories, setCategories] = useState([]);
+
+useEffect(() => {
+  console.log("mounted");
+  getCategories();
+},[]);
+
+const getCategories = async() => {
+  let response = await axios.get("/api/categories");
+  setCategories(response.data)
+};
+
+const renderCategories = () => {
+  if (categories.length === 0) {
+    return <p>No Categories</p>
+  }
+  return categories.map((category) => {
+    return <Category key = {category.id}{...category}/>;
+  });
+};
+
   return (
     <div>
       <h1>Categories Here</h1>
+      {renderCategories()}
     </div>
   );
 };
