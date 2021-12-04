d015117ce504b63df43d293db7f74a3ffb30d04f
Michael Knutson <mrknutson44@gmail.com>
2021-12-04 14:11:05 -0900

    item show working

--- client/src/components/ItemShow.js
+++ client/src/components/ItemShow.js
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router";
+import { useNavigate, useParams } from "react-router";
 import axios from "axios"
 
 const ItemShow = () => {
   const [item, setItem]= useState([]);
   const [category, setCategory]=useState([]);
   const params = useParams();
+  const navigate = useNavigate();
 
   useEffect(()=>{
     getData();
@@ -24,13 +25,18 @@ const ItemShow = () => {
     } 
   };
 
+  const navigateTo = () => {
+    navigate(`/categories/${params.category_id}/items`)
+  };
+
   return (
-    <div classname ="card">
-      <h1 classname = "items-header">Category: {category.name}</h1>
+    <div className ="card">
+      <h1 className = "items-header">Category: {category.name}</h1>
       <div>
         <h3>Item Name: {item.name}</h3>
         <p>Price: ${item.price}</p>
         <p>{item.description}</p>
+        <button onClick={()=>navigateTo()}>Return to all items</button>
       </div>
     </div>
   );
