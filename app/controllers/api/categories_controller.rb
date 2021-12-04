e775125d938086f51856ddec267396074561a9cb
Michael Knutson <mrknutson44@gmail.com>
2021-12-04 07:54:13 -0900

    Backend controllers for Job and Category models complete

--- app/controllers/api/categories_controller.rb
+++ app/controllers/api/categories_controller.rb
@@ -1,2 +1,46 @@
 class Api::CategoriesController < ApplicationController
+  before_action :set_category, only: [:show, :update, :destroy]
+
+  def index
+    render json: Category.all
+  end
+
+  def show
+    render json: @category
+  end
+
+  def create
+    @category=Category.new(category_params)
+   if (@category.save)
+    render json: @category
+   else
+    render json: { errors: @category.errors }, status: :unprocessable_entity
+   end 
+  end
+
+  def update
+    if (@category.update(category_params))
+      render json: @category
+     else
+      render json: { errors: @category.errors }, status: :unprocessable_entity
+     end 
+  end
+
+  def destroy
+    render json: @category.destroy
+  end
+
+
+
+  private
+
+  def set_category
+    @category=Category.find(params[:id])
+  end
+
+  def category_params
+    params.require(:category).permit(:name, :description)
+  end
+
+
 end
--- app/controllers/api/jobs_controller.rb
+++ app/controllers/api/jobs_controller.rb
@@ -1,2 +1,45 @@
 class Api::JobsController < ApplicationController
+  before_action :set_job, only: [:show, :update, :destroy]
+
+  def index
+    render json: Job.all
+  end
+
+  def show
+    render json: @job
+  end
+
+  def create
+    @job=Job.new(job_params)
+   if (@job.save)
+    render json: @job
+   else
+    render json: { errors: @job.errors }, status: :unprocessable_entity
+   end 
+  end
+
+  def update
+    if (@job.update(job_params))
+      render json: @job
+     else
+      render json: { errors: @job.errors }, status: :unprocessable_entity
+     end 
+  end
+
+  def destroy
+    render json: @job.destroy
+  end
+
+
+
+  private
+
+  def set_job
+    @job=Job.find(params[:id])
+  end
+
+  def job_params
+    params.require(:job).permit(:title, :company, :salary)
+  end
+
 end
